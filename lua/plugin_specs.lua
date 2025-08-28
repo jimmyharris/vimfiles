a1c8bd2fdbb7732d8acd7fc7c3fa204c30f97315
James Harris <james.harris@garmin.com>
2025-08-28 09:55:34 -0500

    venv-selector: bump back to main branch

--- lua/plugin_specs.lua
+++ lua/plugin_specs.lua
@@ -402,14 +402,14 @@ local plugin_specs = {
   { "pearofducks/ansible-vim" },
 
   -- python dev tools
-  { "linux-cultist/venv-selector.nvim",
+  {
+    "linux-cultist/venv-selector.nvim",
     dependencies = {
       "neovim/nvim-lspconfig",
       "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", --optional
       "nvim-telescope/telescope.nvim",
     },
     lazy = false,
-    branch = "regexp", -- This is the regexp branch, use this for the new version
     keys = {
       { ",v", "<cmd>VenvSelect<cr>" },
     },
